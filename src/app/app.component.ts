1b606a4abc2d1398c04546bf9eb50a83ceb8caf7
shrivastavashubh <shrivastavashubham103@gmail.com>
2021-07-29 12:15:06 +0530

    googlemaps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { MapsAPILoader} from '@agm/core';
+import { Component,ElementRef,NgZone,OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'googlemap';
+export class AppComponent implements OnInit{
+  title: string = 'AGM project';
+  latitude: any;
+  longitude:any;
+  zoom:any;
+  address:any;
+  geoCoder:any;
+  @ViewChild('search')
+  searchElement!: ElementRef;
+  constructor(private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone)
+  {}
+ngOnInit()
+{
+  this.setCurrentLocation();
+  this.mapsAPILoader.load().then(() => {
+    this.setCurrentLocation();
+    this.geoCoder = new google.maps.Geocoder;
+
+    let autocomplete = new google.maps.places.Autocomplete(this.searchElement.nativeElement);
+    autocomplete.addListener("place_changed", () => {
+      this.ngZone.run(() => {
+        //get the place result
+        let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+        //verify result
+        if (place.geometry === undefined || place.geometry === null) {
+          return;
+        }
+
+        //set latitude, longitude and zoom
+        this.latitude = place.geometry.location.lat();
+        this.longitude = place.geometry.location.lng();
+        this.zoom = 12;
+      });
+    });
+  });
+}
+setCurrentLocation() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.zoom = 15;
+    });
+  }
+}
+markerDragEnd($event:google.maps.MouseEvent) {
+  console.log($event);
+  this.latitude = $event.latLng.lat();
+  this.longitude = $event.latLng.lng();
+  this.getAddress(this.latitude, this.longitude);
+}
+
+getAddress(latitude:any, longitude:any) {
+  this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results:any, status:any) => {
+    console.log(results);
+    console.log(status);
+    if (status === 'OK') {
+      if (results[0]) {
+        this.zoom = 12;
+        this.address = results[0].formatted_address;
+      } else {
+        window.alert('No results found');
+      }
+    } else {
+      window.alert('Geocoder failed due to: ' + status);
+    }
+
+  });
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgmCoreModule } from '@agm/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCaKbVhcX_22R_pRKDYuNA7vox-PtGaDkI',
+      libraries: ['places']
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
